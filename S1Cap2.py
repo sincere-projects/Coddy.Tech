968138429ddb56c7ed06227a3348a0ebffd5db94
Sincere <246142246+sincere-projects@users.noreply.github.com>
2025-11-27 15:47:07 -0300

    update_adicionando_mais

---
+++ S1Cap2.py
@@ -0,0 +1,56 @@
+'''
+-> Váriaveis
+De acordo com a plataforma são contêiners que armazenam
+valores de dados. Em outra palavras guardam os dados que desejo
+em certo local da memória para que possam ser utilizados durante
+o código.
+
+Obs.: É especificado que cada variável tem um NOME único e um 
+valor que pode ser de diferentes tipos.
+'''
+# Númerico
+# Criando uma variável
+var = 5
+
+# Exibindo a mensagem da variável
+print(f'var = {var}')
+
+'''
+Obs.: Existe tipos de números, sendo Int e Float.
+
+Int de Inteiro: 1 ou -2 
+Float, outras palavras números quebrados: 1.32 ou 0.98
+'''
+
+a = 3
+b = 13.2
+
+# Vou testar algo estranho 
+
+print(f'Soma: {a+b}')
+# Deu certo, somou essa loucura...
+
+# String
+# Não era mais fácil ser texto? 
+
+s1 = 'Isto é um texto'
+s2 = "Isto é um outro texto"
+
+print(f'Texto junto: {s1}, {s2}')
+# E olha que String significa CORDA! Que coisa louca.
+
+
+# Boolean 
+# Tipo bool, existe apenas dois valores: true or false
+
+varT = True
+varF = False
+
+# Fui muleque e usei a letra minuscula no começo
+# Não cometam o mesmo erro que eu, faça Tzão ou Fzão 
+print(f'Exibindo: {varT} or {varF}')
+
+"""
+Pausa pro café, cabo minhas energias da conta
+27/11/2025 - 15:46
+"""

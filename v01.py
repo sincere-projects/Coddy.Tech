8f299075b1c7408b5cf8c7184914e87ca8af1e30
Sincere <246142246+sincere-projects@users.noreply.github.com>
2025-11-27 15:26:58 -0300

    Adicionando comentário

--- v01.py
+++ v01.py
@@ -1,4 +1,9 @@
+"""
+Comentário maior, enfim... Não tenho menor ideia de inglés além do básico.
+"""
 print("Hello World!")
-
+# Comentário simples
 print("Primeiro código em Python!")
 print("Olá mundo e olá Python!")
+
+

fa19c16ce571041681b3e22e5f47208467282a46
Sincere <246142246+sincere-projects@users.noreply.github.com>
2025-11-29 11:29:38 -0300

    energiaCabo
    estou pensando em atualizar apenas a cada setes dias isso aqui

--- Python/S1Cap3.py
+++ Python/S1Cap3.py
@@ -18,3 +18,49 @@ print(f"x = {x}, y = {y}, z = {z}")
 
 # Cabo a energia... Pausa pro outro dia ou daqui 3/4hrs
 # 28/11/2025 - 16:45
+
+# Bom dia, iniciandos os estudos agora... To morrendo de sono 07:53 - 29/11/2025
+# Operador Módulo
+# a utilização de % para ter o resultado sendo da conta na divisão que sobra
+# em outra palavras, aquilo que sobrar "o resto" na divisão, é o resultado.
+
+a = 9
+b = 2
+c = 11
+d = a % 2  # Resto 1
+e = b % 3  # Resto 2
+f = c % 10 # Resto 1
+
+print(f"a = {a}")
+print(f"b = {b}")
+print(f"c = {c}")
+print(f"d = {d}")
+print(f"e = {e}")
+print(f"f = {f}")
+
+# Desafio 
+"""
+Escreva um código que inicialize três variáveis, x, y e z com os valores 15, 4 e 23 (respectivamente).
+Depois disso, inicialize as seguintes variáveis:
+
+w que conterá o resultado da divisão inteira de x por y 
+v que conterá o resultado da divisão inteira de z por x
+u que conterá o resultado da divisão inteira de z por y
+Use o operador // para divisão inteira. Por exemplo: 7 // 2 é igual a 3 (não 3.5)
+"""
+x = 15
+y = 4
+z = 23
+w = x // y
+v = z // x
+u = z // y
+
+print(f"x = {x}")
+print(f"y = {y}")
+print(f"z = {z}")
+print(f"w = {w}")
+print(f"v = {v}")
+print(f"u = {u}")
+
+# Energia acabou, partiu próxima aula
+# Atividade OK - 08:05
